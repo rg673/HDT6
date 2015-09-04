25cce28b6a830a2ea1a2005e3ec70c18d8e0cf46
jonagr7 <agu14349@uvg.edu.gt>
2015-09-03 19:56:56 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
 import java.util.*;
+/**
+ * 
+Jonathan Aguirre 14349
+Samuel Diaz 14083
+Laboratorio No. 6
+03/09/2015
+Algoritmos y estructuras de Datos
+ */
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan;
@@ -8,11 +16,11 @@ public class Main {
         System.out.println("Jonthan Aguirre \nSamuel Diaz\nHoja de Trabajo No.6\nElija la implementacion SET: "); // println de los encabezados del menu
         while (op==0){               
                 System.out.println(" 1. HashSet; 2. TreeSet o 3. LinkedHashSet");
-                System.out.print(" Ingrese el n鷐ero: ");            
+                System.out.print(" Ingrese el n煤mero: ");            
                 try{sel=scan.nextInt();}    // lectura de caracter ingresado
                 catch(Exception e){System.out.println();} 
                 op = sel;  
-                if (sel>3||sel<1){ // condici髇 de error en el ingreso de un caracter
+                if (sel>3||sel<1){ // condici贸n de error en el ingreso de un caracter
                     System.out.print("Ingrese una opccion valida");  
                 }
             }
@@ -30,13 +38,13 @@ public class Main {
       while(est == true){ // ciclo
           System.out.println("Ingrese el nombre: ");
           name = scan.next(); // lee el ingreso del nombre
-          System.out.println("__________________________________Primera condici髇________________________________________________");
+          System.out.println("__________________________________Primera condici贸n________________________________________________");
           System.out.println("Responda ingresando s y n, o utilize Si o No");
-          System.out.println("縀l programador es un desarrollador de Java?"); 
+          System.out.println("驴El programador es un desarrollador de Java?"); 
           est2 = true;
           while(est2 == true){
            cont = scan.next(); // lectura de ingreso
-           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){ // condici髇 de ingreso correcto con si
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){ // condici贸n de ingreso correcto con si
               c1.setterop(name);
               o1.setterop(name);
               est2 = false;
@@ -50,12 +58,12 @@ public class Main {
                 }
            } 
           }
-          System.out.println("______________________________________Segunda condici髇__________________________________________");
-          System.out.println("縀l programador es un desarrollador web?");
+          System.out.println("______________________________________Segunda condici贸n__________________________________________");
+          System.out.println("驴El programador es un desarrollador web?");
           est2 = true; // segundo estado activo
           while(est2 == true){
            cont = scan.next();    
-           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condici髇 de ingreso correcto con si
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condici贸n de ingreso correcto con si
               c2.setterop(name);
               o2.setterop(name);
               est2 = false;    
@@ -69,12 +77,12 @@ public class Main {
              }           
             }
           }      
-          System.out.println("_________________________________________Tercera condici髇_________________________________________");
-          System.out.println("縀l programador es un desarrollador de celulares?");
+          System.out.println("_________________________________________Tercera condici贸n_________________________________________");
+          System.out.println("驴El programador es un desarrollador de celulares?");
           est2 = true;    
           while(est2 == true){
            cont = scan.next();  
-           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condici髇 de ingreso correcto con si   
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condici贸n de ingreso correcto con si   
               c3.setterop(name);
               o3.setterop(name);
               est2 = false;    
@@ -89,11 +97,11 @@ public class Main {
             }
           }    
           System.out.println("__________________________________________________________________________________");
-          System.out.println("緿esea Ingresar otro Desarrollador?"); // condici髇 de ingresar otro 
+          System.out.println("驴Desea Ingresar otro Desarrollador?"); // condici贸n de ingresar otro 
           est2=true;     
           while( est2 == true){
            cont = scan.next();    
-           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condici髇 de ingreso correcto con si     
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condici贸n de ingreso correcto con si     
               est2= false;
              } 
            else{
@@ -108,17 +116,17 @@ public class Main {
           }
         }  
          System.out.println("__________________________________________________________________________________");
-              Set co1=c1.getterc();
+              Set co1=c1.getterc(); // Los co contienen los conjuntos alamacenados en los c
               Set co2=c2.getterc();
               Set co3=c3.getterc();
-              Set oo1=o1.getterc();
+              Set oo1=o1.getterc(); // los oo contienen a los conjuntos ordenados dentro de o
               Set oo2=o2.getterc();
               Set oo3=o3.getterc();
               System.out.println("Lista de programadores de Java: "+ co1);
               System.out.println("Lista de programadores de Web: "+co2);
               System.out.println("Lista de programadores de Celulrares: "+co3);
               System.out.println("__________________________________________________________________________________");
-              System.out.println("1.Los desarrolladores con experiencia en Java, web y celulares son: "+ c1.retain(co1,co2,co3)); // se utiliza la funci髇 retain entre los tres conjuntos
+              System.out.println("1.Los desarrolladores con experiencia en Java, web y celulares son: "+ c1.retain(co1,co2,co3)); // se utiliza la funci贸n retain entre los tres conjuntos
               System.out.println("__________________________________________________________________________________");
               System.out.println("2.Los desarrolladores con experiencia en Java pero que no tengan experiencia en web son: "+c1.inclusion(co1,co2)); // se utiliza la inclucion entre los conjuntos 1 y 2
               System.out.println("__________________________________________________________________________________");
@@ -126,14 +134,14 @@ public class Main {
               System.out.println("__________________________________________________________________________________");
               System.out.println("4.Los desarroladores de Web y Celuares, sin experiencia en java son: "+c1.inclusion(c2.add(co2, co3),co1));//se utiliza la inclucion entre la suma del conjunto 2 y 3 y el conjunto 1
               System.out.println("__________________________________________________________________________________");
-              if (c1.sub(co1, co3)==true){ // condici髇 de subconjunto del 1 en el 3
+              if (c1.sub(co1, co3)==true){ // condici贸n de subconjunto del 1 en el 3
                   System.out.println("5. Java es subconjunto del conjunto de desarrolladores Web.");
               }
              else{
                   System.out.println("5. Java no es subconjunto del conjunto de desarrolladores Web.");
               }
               System.out.println("__________________________________________________________________________________");
-              System.out.println("6. El conjunto de mayor tama駉 es: "+ c1.cm(co1, co2, co3)+ "Con nombre "+c1.gettercm()); // se muestra  el conjunto mayor y sus integrantes
+              System.out.println("6. El conjunto de mayor tama帽o es: "+ c1.cm(co1, co2, co3)+ "Con nombre "+c1.gettercm()); // se muestra  el conjunto mayor y sus integrantes
               System.out.println("__________________________________________________________________________________");
               System.out.println("7. Lista de nombres en orden son "+o1.cm(oo1, oo2, oo3)+" del grupo: "+c1.gettercm()); // se despliegan los nombre del conjunto con mayor cantidad de integrantes
     }
