6746e9fd1e2affc2c2fcbac910c1a3b57c53e758
rg673 <samydi673@gmail.com>
2015-09-03 18:44:52 -0600

    Primer version

---
+++ Hashtable.java
@@ -0,0 +1,113 @@
+/**
+ * 
+Jonathan Aguirre 14349
+Samuel Diaz 14083
+
+ */
+/**
+ * 
+ *
+ */
+import java.util.*;
+public class Hashtable {
+	public class Factory { // classe factory
+	    public Set<String> getterStack (int op){
+	        
+	        if (op==1){ // opcion 1 uso de Hashset
+	            return new HashSet<String>();
+	        }
+	        
+	        if (op==2){ // opcion 2 uso de Arboles
+	            return new TreeSet<String>();
+	        }
+	        if (op==3){ // opcion 3 uso de Hashset ensda
+	            return new LinkedHashSet<String>();
+	        }        
+	        return null;       // condición de error 
+	    }
+	}
+   Factory operador = new Factory();  // operador de tipo factory
+   private Set<String> c; // variable para almacenar un conjunto
+   private Set<String> sc; // variable para almacenar un conjunto
+   private String cm; // variable para almacenar el conjunto mayor
+   
+   public Hashtable(int op){// constructor
+        c=operador.getterStack(op);  // conjunto base
+        sc=operador.getterStack(op); // segundo conjunto
+   }
+   public void setterop(String elemento){ // setter del cojunto
+       c.add(elemento);       
+   }
+   public Set<String> getterc(){ // setter del conjunto 
+       return c;
+   }
+   
+   public Set<String> retain(Set<String> c1,Set<String> c2,Set<String> c3){ // intersección de los 3 subconjuntos
+       sc.clear();
+       sc.addAll(c1);
+       sc.retainAll(c2);
+       sc.retainAll(c3);
+       return sc;
+   }
+      
+   public Set<String> inclusion(Set<String> c1,Set<String> c2){ // alamacena en un subconjunto los elementos iguales de dos conjuntos
+       sc.clear();
+       sc.addAll(c1);
+       sc.removeAll(c2);
+       return sc;
+   }
+   
+   public Set<String> retain2(Set<String> c1,Set<String> c2){// interseccion de dos conjuntos
+       sc.clear();
+       sc.addAll(c1);
+       sc.retainAll(c2);
+       return sc;
+   }
+   public Set<String> add(Set<String> c1,Set<String> c2){// suma de dos conjuntos
+       sc.clear();
+       sc.addAll(c1);
+       return sc;
+   }
+   public boolean sub(Set<String> c1,Set<String> c2){ // condición de subconunto de otro conjunto
+       sc.clear();
+       sc.addAll(c1);
+       sc.retainAll(c2);
+       if (sc.size()==c1.size()){
+           return true;
+       }
+       else{
+           return false;
+       }
+   }
+   public Set<String> cm(Set<String> c1,Set<String> c2,Set<String> c3){// devuleve el nombre conjunto con mayor tamaño
+       sc.clear();
+       if (c1.size()>=c2.size()){
+           sc.addAll(c1);
+           cm="desarrolladores de Java";
+       }
+       else{
+           sc.addAll(c2);
+           cm="desarrolladores Web";
+       }
+       if (sc.size()>=c3.size()){
+           return sc;
+       }
+       else{
+          cm="desarrolladores de Celulares";
+          return c3; 
+       }
+
+   }
+   public String gettercm() { // getter del conjunto más grande
+        return cm;
+    }
+   public String getterc(Set<String> c1){  
+       String s= c1.toString();
+       s=s.replace("{", "");
+       s=s.replace("}", " ");
+       s=s.replace(";", "\n");
+       return s;
+       
+   }
+   
+}
---
+++ Main.java
@@ -0,0 +1,131 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan;
+        int op = 0;
+        int sel= 0;
+        scan = new Scanner (System.in); // scaner para leer los enteros
+        System.out.println("Jonthan Aguirre \nSamuel Diaz\nHoja de Trabajo No.6\nElija la implementacion SET: "); // println de los encabezados del menu
+        while (op==0){               
+                System.out.println(" 1. HashSet; 2. TreeSet o 3. LinkedHashSet");
+                System.out.print(" Ingrese el número: ");            
+                try{sel=scan.nextInt();}    // lectura de caracter ingresado
+                catch(Exception e){System.out.println();} 
+                op = sel;  
+                if (sel>3||sel<1){ // condición de error en el ingreso de un caracter
+                    System.out.print("Ingrese una opccion valida");  
+                }
+            }
+      System.out.println("__________________________________________________________________________________");
+      boolean est = true; // estado del ciclo de ingreso de usuarios
+      boolean est2; // segundo estado dentro del ciclo
+      Hashtable c1 = new Hashtable(op); // Conjunto de java
+      Hashtable c2 = new Hashtable(op); // conjunto de web
+      Hashtable c3 = new Hashtable(op); // conjunto de moviles
+      String cont = "";  
+      String name = ""; // nombre del programador
+      while(est == true){ // ciclo
+          System.out.println("Ingrese el nombre: ");
+          name = scan.next(); // lee el ingreso del nombre
+          System.out.println("__________________________________Primera condición________________________________________________");
+          System.out.println("Responda ingresando s y n, o utilize Si o No");
+          System.out.println("¿El programador es un desarrollador de Java?"); 
+          est2 = true;
+          while(est2 == true){
+           cont = scan.next(); // lectura de ingreso
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){ // condición de ingreso correcto con si
+              c1.setterop(name);
+              est2 = false;
+             } 
+           else{
+                if((cont.toLowerCase().equals("no")) || (cont.toLowerCase().equals("n"))){ // condicion correcta con no
+                    est2 = false;
+                }     
+           else{ // ingreso incorrecto
+                    System.out.println("Ingreso incorrecto; solo responda ingresando s y n, o utilize Si o No");
+                }
+           } 
+          }
+          System.out.println("______________________________________Segunda condición__________________________________________");
+          System.out.println("¿El programador es un desarrollador web?");
+          est2 = true; // segundo estado activo
+          while(est2 == true){
+           cont = scan.next();    
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condición de ingreso correcto con si
+              c2.setterop(name);
+              est2 = false;    
+             } 
+           else{
+        	   if((cont.toLowerCase().equals("no")) || (cont.toLowerCase().equals("n"))){ // condicion correcta con no
+              est2 = false;
+                }          
+           else{ // ingreso incorrecto
+                    System.out.println("Ingreso incorrecto; solo responda ingresando s y n, o utilize Si o No");
+             }           
+            }
+          }      
+          System.out.println("_________________________________________Tercera condición_________________________________________");
+          System.out.println("¿El programador es un desarrollador de celulares?");
+          est2 = true;    
+          while(est2 == true){
+           cont = scan.next();  
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condición de ingreso correcto con si   
+              c3.setterop(name);
+              est2 = false;    
+             }    
+           else{
+        	  if((cont.toLowerCase().equals("no")) || (cont.toLowerCase().equals("n"))){ // condicion correcta con no
+                    est2 = false;
+                }
+           else{ // ingreso incorrecto
+                  System.out.println("Ingreso incorrecto; solo responda ingresando s y n, o utilize Si o No");
+           	  } 
+            }
+          }    
+          System.out.println("__________________________________________________________________________________");
+          System.out.println("¿Desea Ingresar otro Desarrollador?"); // condición de ingresar otro 
+          est2=true;     
+          while( est2 == true){
+           cont = scan.next();    
+           if((cont.toLowerCase().equals("si")) || (cont.toLowerCase().equals("s"))){// condición de ingreso correcto con si     
+              est2= false;
+             } 
+           else{
+        	   if((cont.toLowerCase().equals("no")) || (cont.toLowerCase().equals("n"))){ // condicion correcta con no
+                    est2 = false;
+                     est = false; // para salir del ciclo se cambia el estado
+                }
+           else{ // ingreso incorrecto
+                   System.out.println("Ingreso incorrecto; solo responda ingresando s y n, o utilize Si o No");
+            	  }
+           }
+          }
+        }  
+         System.out.println("__________________________________________________________________________________");
+              Set co1=c1.getterc();
+              Set co2=c2.getterc();
+              Set co3=c3.getterc();
+              System.out.println("Lista de programadores de Java: "+ co1);
+              System.out.println("Lista de programadores de Web: "+co2);
+              System.out.println("Lista de programadores de Celulrares: "+co3);
+              System.out.println("__________________________________________________________________________________");
+              System.out.println("1.Los desarrolladores con experiencia en Java, web y celulares son: "+ c1.retain(co1,co2,co3)); // se utiliza la función retain entre los tres conjuntos
+              System.out.println("__________________________________________________________________________________");
+              System.out.println("2.Los desarrolladores con experiencia en Java pero que no tengan experiencia en web son: "+c1.inclusion(co1,co2)); // se utiliza la inclucion entre los conjuntos 1 y 2
+              System.out.println("__________________________________________________________________________________");
+              System.out.println("3.Los desarrolladores de Web o Celuares, sin experiencia en java son: "+c1.inclusion(c2.retain2(co2,co3), co1));// se utiliza la inclucion entre lo retenido en dos conjuntos(2 y 3) y otro
+              System.out.println("__________________________________________________________________________________");
+              System.out.println("4.Los desarroladores de Web y Celuares, sin experiencia en java son: "+c1.inclusion(c2.add(co2, co3),co1));//se utiliza la inclucion entre la suma del conjunto 2 y 3 y el conjunto 1
+              System.out.println("__________________________________________________________________________________");
+              if (c1.sub(co1, co3)==true){ // condición de subconjunto del 1 en el 3
+                  System.out.println("5. Java es subconjunto del conjunto de desarrolladores Web.");
+              }
+             else{
+                  System.out.println("5. Java no es subconjunto del conjunto de desarrolladores Web.");
+              }
+              System.out.println("__________________________________________________________________________________");
+              System.out.println("6. El conjunto de mayor tamaño es: "+ c1.gettercm()+ " "+c1.cm(co1, co2, co3) ); // se muestra  el conjunto mayor y sus integrantes
+              System.out.println("__________________________________________________________________________________");
+              System.out.println("7. Lista de nombres de "+c1.gettercm()+" en orden es: "+c1.getterc(c2.cm(co1, co2, co3))); // se despliegan los nombre del conjunto con mayor cantidad de integrantes
+    }
+}
